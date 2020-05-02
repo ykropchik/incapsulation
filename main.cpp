a35e9a7df9c2fef1b039208cf0b13eb681a4d678
ykropchik22 <40296340+ykropchik@users.noreply.github.com>
2020-05-02 12:11:10 +1000

    Added second task

--- main.cpp
+++ main.cpp
@@ -3,65 +3,55 @@
 class Phone {
 protected:
     std::string name;
-    std::string serialNumber;
 public:
     std::string brandName;                      // Отсутствие инкапсуляции
 
-    // Инкапсуляция через позднее связывание
-    virtual void callRequest(Phone *object){
+    Phone(const std::string &name, const std::string &brandName){
+        this->name = name;
+        this->brandName = brandName;
+    };
+
+    virtual std::string setName(const std::string &name);
+    virtual std::string getName();
+};
+
+class MobilePhone : public Phone {
+private:
+    std::string serialNumber;
+public:
+    // Расширение
+    MobilePhone(const std::string &name, const std::string &serialNumber, const std::string &brandName): Phone(name, brandName) {
+        this->serialNumber = serialNumber;
+    }
+
+    // Замена поведения
+    std::string setName(const std::string &name) override {                      // Инкапсуляция при помощи сетера
+        MobilePhone::name = name;
+    }
+
+    std::string getName() override {                                             // Инкапсуляция при помощи гетера
+        return MobilePhone::name;
+    }
+
+    // Позднее связывание
+    virtual void callRequest(MobilePhone *object){
         std::cout << this->serialNumber << ": Waiting answer...\n";
         object->callAnswer(this);
     };
 
 private:
-    virtual void callAnswer(Phone *phone) {
+    virtual void callAnswer(MobilePhone *phone) {
         std::cout << this->serialNumber << ": ";
         std::cout << phone->serialNumber << " Request a call\n";
         std::cout << this->serialNumber << ": " << "Request approved\n";
         std::cout << this->serialNumber << ": " << "Start call\n";
     }
-};
-
-class MobilePhone : public Phone {
-public:
-    MobilePhone(const std::string &name, const std::string &serialNumber, const std::string &brandName) {
-        this->name = name;
-        this->serialNumber = serialNumber;
-        this->brandName = brandName;
-    }
-
-    std::string setName(const std::string &name){                      // Инкапсуляция при помощи сетера
-        MobilePhone::name = name;
-    }
-
-    std::string getName(){                                             // Инкапсуляция при помощи гетера
-        return MobilePhone::name;
-    }
-
-};
-
-class HomePhone : public Phone {
-public:
-    HomePhone(const std::string &name, const std::string &serialNumber, const std::string &brandName) {
-        this->name = name;
-        this->serialNumber = serialNumber;
-        this->brandName = brandName;
-    }
-
-    std::string setName(const std::string &name){                      // Инкапсуляция при помощи сетера
-        HomePhone::name = name;
-    }
-
-    std::string getName(){                                             // Инкапсуляция при помощи гетера
-        return HomePhone::name;
-    }
-
 
 };
 
 int main() {
     MobilePhone firstPhone = MobilePhone("mYpHonE", "7SY371D", "Nokia");
-    HomePhone secondPhone = HomePhone("HomePhoneROSTELECOM", "612HS73G", "Panasonic");
+    MobilePhone secondPhone = MobilePhone("Phone3000", "js8sG2g", "Samsung");
 
     firstPhone.callRequest(&secondPhone);
     return 0;
